477b26ca4b5c278f5cd17ba337cf911b712c2d8b
AHeraldOfTheNewAge <55515171+AHeraldOfTheNewAge@users.noreply.github.com>
2022-02-16 15:35:57 +0200

    is image?
    We check if the links inserted into the command line are actual images

--- taggus.rs
+++ taggus.rs
@@ -1,10 +1,27 @@
 use std::env;
 use std::path::Path;
 
-fn is_image(img:&str) -> bool{
-    let file_is = Path::new(img).is_file();
+/**
+    Loosely check if file is an image
+*/
+fn is_image(img:String) -> bool{
+    let approved_file_types = vec!["png", "jpg", "jpeg"];
+    let mut is_approved_file_type = false;
 
-    if !file_is {
+    for file_type in approved_file_types {
+        if img.ends_with(file_type) {
+            is_approved_file_type = true;
+            break;
+        }
+    }
+
+    if !is_approved_file_type {
+        return false;
+    }
+
+    let file_exists = Path::new(img.as_str()).is_file();
+
+    if !file_exists {
         return false;
     }
 
@@ -12,11 +29,11 @@ fn is_image(img:&str) -> bool{
 }
 
 fn main() {
-    let mut images: Vec<_> = env::args().collect();
+    let mut images: Vec<String> = env::args().collect();
     images.remove(0);//remove the call to this file..
 
     for image in images{
-        println!("imagine? {}", is_image(image.as_str()));
+        println!("imagine? {}", is_image(image));
     }
 
     //println!("--------------");
